5fff6c6c2e31bf758239bb870d0d1409156abcd5
Jonathan Lu <jzlu@CNQYLWCQXY.tld>
2025-06-04 18:02:51 -0400

    setup search infra

--- search.py
+++ search.py
@@ -18,3 +18,73 @@ Stage 2 (practicality):
     * Evaluate the circuit distance?
 """
 import numpy as np
+from helix import find_stabilizers
+from distance import distance
+
+def is_canonical(helix_code):
+    """
+    Check whether `helix_code` is in canonical form.
+    We say that a helix code (group, X0, Z0) is in canonical form if TODO
+    
+    Input:
+        * `helix_code` (tuple): Tuple of (group, X0, Z0) where group is a tuple and X0 and Z0 are sets
+    
+    Returns:
+        * Binary indication of whether `helix_code` is in canonical form.
+    """
+    pass
+
+def passes_stage_1(helix_code):
+    """
+    Evaluate whether a given code passes through stage 1 of tests.
+
+    Input:
+        * `helix_code` (tuple): Tuple of (group, X0, Z0) where group is a tuple and X0 and Z0 are sets
+    
+    Returns:
+        * Binary indication of whether `helix_code` is in canonical form.
+    """
+    pass
+
+def group_level_search(group, X_wt, Z_wt):
+    """
+    Search helix codes formed from `group` with X-weight `X_wt` and Z-weight `Z_wt`.
+    Save the ones which pass stage 1.
+
+    Input:
+        * group (tuple): tuple of integers specifying the group
+        * X_wt (int): weight of the X's in each check
+        * Z_wt (int): weight of the Z's in each check
+    
+    Output:
+        * A .npy file for each code which passes the stage 1 test
+    
+    Returns:
+        * None
+    """
+    pass
+
+def n_level_search(n, X_wt, Z_wt):
+    """
+    Search helix codes over `n` qubits with X-weight `X_wt` and Z-weight `Z_wt`.
+    Save the ones which pass stage 1.
+
+    Input:
+        * n (int): number of qubits
+        * X_wt (int): weight of the X's in each check
+        * Z_wt (int): weight of the Z's in each check
+    
+    Output:
+        * A .npy file for each code which passes the stage 1 test
+    
+    Returns:
+        * None
+    """
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
