86fdcca7984a481daf6682b3093da087cd91e812
AndreyBorisKhesin <andrey.khesin@gmail.com>
2025-06-11 21:09:42 -0300

    computed code indexing, with temporary functionality to just count the number of valid codes

--- search.py
+++ search.py
@@ -18,43 +18,51 @@ Stage 2 (practicality):
     * Evaluate the circuit distance?
 """
 import numpy as np
-from helix import find_stabilizers
+import itertools as it
+from primefac import primefac
+from helix import canonicalize, find_stabilizers
 from distance import distance
+
+def partitions(n, I=1):
+    yield (n,)
+    for i in range(I, n//2 + 1):
+        for p in partitions(n-i, i):
+            yield (i,) + p
 
 def is_canonical(helix_code):
     """
     Check whether `helix_code` is in canonical form.
-    We say that a helix code (group, X0, Z0) is in canonical form if TODO
+    We say that a helix code (group, Z0, X0) is in canonical form if TODO
     
     Input:
-        * `helix_code` (tuple): Tuple of (group, X0, Z0) where group is a tuple and X0 and Z0 are sets
+        * `helix_code` (tuple): Tuple of (group, Z0, X0) where group is a tuple and Z0 and X0 are sets
     
     Returns:
         * Binary indication of whether `helix_code` is in canonical form.
     """
-    pass
+    return helix_code == canonicalize(*helix_code)
 
 def passes_stage_1(helix_code):
     """
     Evaluate whether a given code passes through stage 1 of tests.
 
     Input:
-        * `helix_code` (tuple): Tuple of (group, X0, Z0) where group is a tuple and X0 and Z0 are sets
+        * `helix_code` (tuple): Tuple of (group, Z0, X0) where group is a tuple and Z0 and X0 are sets
     
     Returns:
-        * Binary indication of whether `helix_code` is in canonical form.
+        * Binary indication of whether `helix_code` passes stage 1 of tests.
     """
     pass
 
-def group_level_search(group, X_wt, Z_wt):
+def group_level_search(group, Z_wt, X_wt):
     """
-    Search helix codes formed from `group` with X-weight `X_wt` and Z-weight `Z_wt`.
+    Search helix codes formed from `group` with Z-weight `Z_wt` and X-weight `X_wt`.
     Save the ones which pass stage 1.
 
     Input:
         * group (tuple): tuple of integers specifying the group
-        * X_wt (int): weight of the X's in each check
         * Z_wt (int): weight of the Z's in each check
+        * X_wt (int): weight of the X's in each check
     
     Output:
         * A .npy file for each code which passes the stage 1 test
@@ -64,15 +72,15 @@ def group_level_search(group, X_wt, Z_wt):
     """
     pass
 
-def n_level_search(n, X_wt, Z_wt):
+def n_level_search(n, Z_wt, X_wt):
     """
-    Search helix codes over `n` qubits with X-weight `X_wt` and Z-weight `Z_wt`.
+    Search helix codes over `n` qubits with Z-weight `Z_wt` and X-weight `X_wt`.
     Save the ones which pass stage 1.
 
     Input:
         * n (int): number of qubits
-        * X_wt (int): weight of the X's in each check
         * Z_wt (int): weight of the Z's in each check
+        * X_wt (int): weight of the X's in each check
     
     Output:
         * A .npy file for each code which passes the stage 1 test
@@ -82,6 +90,90 @@ def n_level_search(n, X_wt, Z_wt):
     """
     pass
 
+def num_indices(n, Z_wt, X_wt):
+    result = 1
+    for i in it.groupby(list(primefac(n))):
+        result *= len(list(partitions(len(list(i[1])))))
+    return result * n ** (Z_wt + X_wt - 1)
+
+def index_to_tuple(group, index):
+    result = []
+    for g in group[::-1]:
+        result.append(index % g)
+        index //= g
+    return (*result[::-1],)
+
+def multi_index_to_tuples(group, Z_wt, X_wt, index):
+    n = int(np.prod(group))
+    Zs = []
+    Xs = []
+    Z_nums = []
+    X_nums = []
+    for _ in range(X_wt):
+        Xs.append(index_to_tuple(group, index))
+        X_nums.append(index % n)
+        index //= n
+    for _ in range(Z_wt - 1):
+        Zs.append(index_to_tuple(group, index))
+        Z_nums.append(index % n)
+        index //= n
+    Zs.append((0,) * len(group))
+    Z_nums.append(0)
+    return Zs[::-1], Xs[::-1], Z_nums[::-1], X_nums[::-1]
+
+def n_partitions(n):
+    primes = []
+    powers = []
+    for i in it.groupby(list(primefac(n))):
+        primes.append(i[0])
+        powers.append(len(list(i[1])))
+    combos = [list(partitions(i)) for i in powers]
+    result = []
+    for i in it.product(*combos):
+        group_sizes = []
+        for j, p in zip(i, primes):
+            for k in j:
+                group_sizes.append(p ** k)
+        result.append((*group_sizes,))
+    return result
+
+'''
+index_end is exclusive
+'''
+def process_codes(n, Z_wt, X_wt, index_start = 0, index_end = None):
+    if index_end == None:
+        index_end = num_indices(n, Z_wt, X_wt)
+    groups = n_partitions(n)
+    index = index_start
+    COUNTER = 0
+    while index < index_end:
+        quotient, remainder = divmod(index, n ** (Z_wt + X_wt - 1))
+        group = groups[quotient]
+        Zs, Xs, Z_nums, X_nums = multi_index_to_tuples(group, Z_wt, X_wt, remainder)
+        jump =  -1
+        for i in range(1, Z_wt + X_wt):
+            if (any([j > 0 and g % j > 0 for j, g in zip(Zs[1], group)]) or
+                (i < Z_wt and Z_nums[i] <= Z_nums[i - 1]) or
+                (i > Z_wt and X_nums[i - Z_wt] <= X_nums[i - Z_wt - 1]) or
+                (i == Z_wt and (max(Xs[0]) > 1 or
+                    any([j > 0 and g % 2 > 0 for j, g in zip(Xs[0], group)])))):
+                jump = n ** (Z_wt + X_wt - 1 - i)
+                break
+        group = np.array(group)
+        Zs = np.array(Zs)
+        Xs = np.array(Xs)
+        if jump < 0:
+            canon_Z, canon_X = canonicalize(group, Zs, Xs)
+            if not (np.all(Zs == canon_Z) and np.all(Xs == canon_X)):
+                jump = 1
+        if jump >= 0:
+            index //= jump
+            index += 1
+            index *= jump
+            continue
+        COUNTER += 1
+        index += 1
+    return COUNTER
 
 def main():
     pass
