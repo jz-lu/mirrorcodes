20a3d8e8ed6aaf824bb9d8da06c19acb88da1f14
AndreyBorisKhesin <andrey.khesin@gmail.com>
2025-06-17 01:08:17 +0100

    added weight arguments and changed timeout to be windows friendly

--- filter.py
+++ filter.py
@@ -24,17 +24,18 @@ process those codes. Therefore, at each stage the next file saved is substantial
 smaller than the previous file saved.
 """
 import argparse
+from func_timeout import func_timeout, FunctionTimedOut
 import numpy as np
 import pickle
+import signal
+
 from constants import get_filename, \
                       RATE_THRESHOLD, DISTANCE_THRESHOLD, \
                       DISTANCE_RATE_THRESHOLD
-
-from util import stimify_symplectic
+from distance import distance
 from helix import HelixCode
 from search import find_all_codes
-from distance import distance
-import signal
+from util import stimify_symplectic
 
 class TimeoutException(Exception):
     pass
@@ -64,7 +65,8 @@ def stage2(n:int, codes:list, verbose:bool=False):
 
     Params:
         * n (int): number of qubits.
-        * codes (list): list of helix codes in ((group, Z_0, X_0), k) form which passed stage 1.
+        * codes (list): list of helix codes in (group, Z_0, X_0, IS_CSS, k) form
+          which passed stage 1.
 
     Returns:
         * list of helix codes in ((group, Z_0, X_0), k) form which pass stage 2.
@@ -86,41 +88,51 @@ def stage3(n:int, codes:list, t:int=3, verbose:bool=False):
 
     Params:
         * n (int): number of qubits.
-        * codes (list): list of helix codes in ((group, Z_0, X_0), k) form which passed stage 2.
-        * t (int): how many seconds you are willing to spend on the distance calculation.
+        * codes (list): list of helix codes in (group, Z_0, X_0, IS_CSS, k) form
+          which passed stage 2.
+        * t (int): how many seconds you are willing to spend on the distance
+          calculation.
 
     Returns:
-        * list of helix codes in (group, Z_0, X_0, k, d, k*d/n) form which pass stage 3.
-          (d -> -1, k*d/n -> -1 if distance failed to calculate in time t)
+        * list of helix codes in (group, Z_0, X_0, k, d, k*d/n) form which pass
+          stage 3. (d -> -1, k*d/n -> -1 if distance failed to calculate in time t)
     """
     passing_codes = []
     seen = set()
     for code_data in codes:
         group, z0, x0, is_css, k = code_data
-        code = HelixCode(group, z0, x0, n=n, k=k, is_css=is_css)
+        code = HelixCode(group, z0, x0, n = n, k = k, is_css = is_css)
         d = -1
-        old_handler = signal.signal(signal.SIGALRM, _timeout_handler)
-        signal.alarm(t)
         try:
-            d = code.get_d()
-        except TimeoutException:
+            d = func_timeout(t, code.get_d)
+        except FunctionTimedOut:
             d = -1
             if verbose:
                 print(f"Distance calculation timed out at {t}s for code {code}, z0 = {z0}, x0 = {x0}")
-        finally:
-            # Clean up: cancel pending alarms & restore the old handler
-            signal.alarm(0)
-            signal.signal(signal.SIGALRM, old_handler)
+        #old_handler = signal.signal(signal.SIGALRM, _timeout_handler)
+        #signal.alarm(t)
+        #try:
+        #    d = code.get_d()
+        #except TimeoutException:
+        #    d = -1
+        #    if verbose:
+        #        print(f"Distance calculation timed out at {t}s for code {code}, z0 = {z0}, x0 = {x0}")
+        #finally:
+        #    # Clean up: cancel pending alarms & restore the old handler
+        #    signal.alarm(0)
+        #    signal.signal(signal.SIGALRM, old_handler)
         goodness = k*d/n
         goodness_str = f" (GR = {round(goodness, 4)})" if d > 0 else ""
-        if d == -1 or (d >= DISTANCE_THRESHOLD and goodness >= DISTANCE_RATE_THRESHOLD):
+        if d == -1 or (d >= DISTANCE_THRESHOLD
+                       and goodness >= DISTANCE_RATE_THRESHOLD):
             if verbose and (k, d) not in seen:
                 # Only print codes with genuinely new parameters.
                 print(f"[[{n}, {k}, {d}]]{goodness_str} code found")
                 if goodness >= 0.9:
                     print(f"*******  Someone has a bright future! *******")
             seen.add((k, d))
-            passing_codes.append((group, z0, x0, is_css, k, d, -1 if d == -1 else round(k*d/n, 5)))
+            passing_codes.append((group, z0, x0, is_css, k, d,
+                                  -1 if d == -1 else round(k*d/n, 5)))
         # else:
         #     if verbose:
         #         print(f"[[{n}, {k}, {d}]]{goodness} code is BAD")
@@ -139,6 +151,12 @@ def main(args):
     out_directory = args.output
     if out_directory is None:
         out_directory = in_directory
+    Z_wt = args.Zweight
+    if Z_wt is None:
+        Z_wt = 3
+    X_wt = args.Xweight
+    if X_wt is None:
+        X_wt = 3
     stage = args.stage
     n = args.size
     print(f"Running: n = {n}")
@@ -146,13 +164,13 @@ def main(args):
 
     if args.fullsend:
         print("[Fullsend] Starting stage 1")
-        out_data = stage1(n, Z_wt=3, X_wt=3, rate_filter=True)
+        out_data = stage1(n, Z_wt = Z_wt, X_wt = X_wt, rate_filter = True)
         print(f"Filtered to {len(out_data)} codes")
         print("[Fullsend] Starting stage 2")
         out_data = stage2(n, out_data)
         print(f"Filtered to {len(out_data)} codes")
         print("[Fullsend] Starting stage 3")
-        out_data = stage3(n, out_data, t=3, verbose=VERBOSE)
+        out_data = stage3(n, out_data, t = 3, verbose = VERBOSE)
         print(f"Filtered to {len(out_data)} codes")
 
         if SAVE_DATA:
@@ -164,7 +182,7 @@ def main(args):
         if stage == 1:
             out_data = stage1(n, 3, 3, rate_filter=True)
         else:
-            in_file = f"{in_directory}/{get_filename(stage-1, n)}"
+            in_file = f"{in_directory}/{get_filename(stage - 1, n)}"
             in_data = None
             with open(in_file, "rb") as f:
                 in_data = pickle.load(f)
@@ -194,6 +212,20 @@ if __name__ == "__main__":
         type=int,
         required=True,
         help="Number of qubits"
+    )
+
+    parser.add_argument(
+        "--Zweight", "-z",
+        type=int,
+        default=3,
+        choices=[2, 3, 4, 5, 6]
+    )
+
+    parser.add_argument(
+        "--Xweight", "-x",
+        type=int,
+        default=3,
+        choices=[2, 3, 4, 5, 6]
     )
 
     parser.add_argument(
